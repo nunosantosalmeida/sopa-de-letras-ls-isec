6ab214685caca742bd099c843402f7008ab70d5d
Renato Craveiro <renatoalex.olivcraveiro@gmail.com>
2022-06-07 02:17:15 +0100

    Comments on letter.component/css
    Comments explaining the letter component. (PT ONLY)

--- src/components/letter/letter.component.jsx
+++ src/components/letter/letter.component.jsx
@@ -1,7 +1,20 @@
+/**
+ * Trabalho Realizado por Micael Eid (2019112744), Nuno Almeida (2019121655) e Renato Craveiro (2018011392)
+ * No âmbitio da Unidade Curricular de Linguagens Script (1º Ano - 2º Semestre)
+ * ISEC - Licenciatura em Engenharia Informática - Ano Letivo 2021/2022  
+ */
+
 import React from 'react';
 import "./letter.css"
-
-function Card({item}){
+/**
+ * Letter
+ * 
+ * Representa uma letra (em jsx) a ser apresentada num tabuleiro
+ * 
+ * @param {*} param0 Letra a ser apresentada
+ * @returns letra em jsx
+ */
+function Letter({item}){
     return (
     <div className="letter">
         {item}
@@ -9,4 +22,4 @@ function Card({item}){
     )
 }
 
-export default Card
+export default Letter
