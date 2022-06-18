d19dde7a0dcd776bedf723eae0da03ef4c4dcf09
Micael Melo <a2019112744@isec.pt>
2022-06-18 03:05:00 +0100

    Layout + att
    correção gameboard

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -6,7 +6,7 @@ function Header() {
       <section id ="header">
         <div className="container-fluid text-center">
           <h1 className="tittle display-4">Sopa de Letras</h1>
-          <h2 className="subtitle">Trabalho Pratico de LS - 21/22</h2>
+          <h2 className="subtitle">Trabalho Prático de LS - 21/22</h2>
         </div>
       </section>
     </header>
